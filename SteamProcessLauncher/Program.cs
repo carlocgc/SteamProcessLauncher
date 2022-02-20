7107c19991d262b927e5417027c576e16a4c3c4c
Carlo Mongiello <carlomongiello@improbable.io>
2022-02-20 19:09:35 +0000

    added start info

--- SteamProcessLauncher/Program.cs
+++ SteamProcessLauncher/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Windows.Forms;
+using System.Text;
 
 /// <summary>
 /// Application that will launch a given process with a given set of arguments
@@ -15,17 +16,25 @@ namespace SkyrimSKSELauncher
         {
             if (args.Length < 2)
             {
-                DialogResult result = MessageBox.Show($"Invalid Args: args[0] = {args[0]}, args[1] = {args[1]}. \nExpected: SkyrimSKSELauncher.exe \"X:\\Modding\\Tools\\SkyrimSE\\Mod Organizer 2\\ModOrganizer.exe\" \"\"\"X:\\Modding\\SteamLibrary\\steamapps\\common\\Skyrim Special Edition\\skse64_loader.exe\"\"\" ",
+                DialogResult result = MessageBox.Show($"Invalid Arguments \nUsage: CMD -> SteamProcessLaucher.exe application.exe arg1 arg2 \n Example: CMD -> SteamProcessLauncher.exe \"\"\"X:\\Modding\\Tools\\SkyrimSE\\Mod Organizer 2\\ModOrganizer.exe\"\"\" \"\"\"X:\\Modding\\SteamLibrary\\steamapps\\common\\Skyrim Special Edition\\skse64_loader.exe\"\"\"",
 "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                
-                    Environment.Exit(1);
+
+                Environment.Exit(1);
             }
             else
             {
                 string application = args[0];
-                string command_args = args[1];
+                StringBuilder arguments = new StringBuilder();
 
-                Process.Start(application, command_args);
+                for (int i = 1; i < args.Length; i++)
+                {
+                    arguments.Append(args[i] + " ");
+                }
+                
+
+                ProcessStartInfo start_info = new ProcessStartInfo(application);
+                start_info.Arguments = arguments.ToString().Trim();
+                _ = Process.Start(start_info);
 
                 Environment.Exit(0);
             }
