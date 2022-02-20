c0af1ec615f84cedc30140ad971b8d0beace7a67
Carlo Mongiello <carlomongiello@improbable.io>
2022-02-20 19:26:08 +0000

    Removed custom arg logic as steam doesnt support command line args

--- SteamProcessLauncher/Program.cs
+++ SteamProcessLauncher/Program.cs
@@ -1,43 +1,15 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Windows.Forms;
-using System.Text;
 
-/// <summary>
-/// Application that will launch a given process with a given set of arguments
-/// e.g SkyrimSKSELauncher.exe """X:\Modding\Tools\SkyrimSE\Mod Organizer 2\ModOrganizer.exe""" """X:\Modding\SteamLibrary\steamapps\common\Skyrim Special Edition\skse64_loader.exe"""
-/// </summary>
-
-namespace SkyrimSKSELauncher
+namespace SteamProcessLauncher
 {
     class Program
     {
         static void Main(string[] args)
         {
-            if (args.Length < 2)
-            {
-                DialogResult result = MessageBox.Show($"Invalid Arguments \nUsage: CMD -> SteamProcessLaucher.exe application.exe arg1 arg2 \n Example: CMD -> SteamProcessLauncher.exe \"\"\"X:\\Modding\\Tools\\SkyrimSE\\Mod Organizer 2\\ModOrganizer.exe\"\"\" \"\"\"X:\\Modding\\SteamLibrary\\steamapps\\common\\Skyrim Special Edition\\skse64_loader.exe\"\"\"",
-"Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            _ = Process.Start(@"""X:\Modding\Tools\SkyrimVR\Mod Organizer 2\ModOrganizer.exe""", @"""X:\Modding\SteamLibrary\steamapps\common\SkyrimVR\skse64_loader.exe""");
 
-                Environment.Exit(1);
-            }
-            else
-            {
-                string application = args[0];
-                StringBuilder arguments = new StringBuilder();
-
-                for (int i = 1; i < args.Length; i++)
-                {
-                    arguments.Append(args[i] + " ");
-                }
-                
-
-                ProcessStartInfo start_info = new ProcessStartInfo(application);
-                start_info.Arguments = arguments.ToString().Trim();
-                _ = Process.Start(start_info);
-
-                Environment.Exit(0);
-            }
+            Environment.Exit(0);
         }
     }
 }
